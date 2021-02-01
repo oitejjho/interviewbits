bfcd8263edcfa8c5d4f52459b2abd982c7949bf5
Oitejjho Dutta <oitejjhodutta@ip-192-168-1-64.ap-southeast-1.compute.internal>
2021-02-01 14:08:40 +0700

    code refactored

--- src/com/company/LargestNumber.java
+++ src/com/company/LargestNumber.java
@@ -1,4 +1,4 @@
-package com.company;
+package company;
 
 import java.util.*;
 
@@ -49,31 +49,6 @@ public class LargestNumber {
 
     }
 
-
-    public static int firstMissingPositive(ArrayList<Integer> A) {
-
-
-        if (A == null)
-            return 1;
-        int size = A.size();
-        for (int i = 0; i < size; i++) {
-            if (A.get(i) < 0)
-                A.set(i, size + 2);
-        }
-        for (int i = 0; i < size; i++) {
-            if (Math.abs(A.get(i)) <= size) {
-                int val = Math.abs(A.get(i));
-                val = Math.abs(A.get(val - 1));
-                A.set(Math.abs(A.get(i)) - 1, -val);
-            }
-        }
-        for (int i = 0; i < size; i++) {
-            if (A.get(i) > 0)
-                return i + 1;
-        }
-        return size + 1;
-
-    }
 
 
 }
