98190175fb461f015cc7b3f405ce81fc771a1e59
Oitejjho Dutta <oitejjhodutta@ip-192-168-1-64.ap-southeast-1.compute.internal>
2021-02-01 13:59:38 +0700

    MinStepsinInfiniteGrid problem.

---
+++ src/com/company/MinStepsinInfiniteGrid.java
@@ -0,0 +1,21 @@
+package company;
+
+public class MinStepsinInfiniteGrid {
+
+    public int coverPoints(int[] A, int[] B) {
+
+        int distance = 0;
+        for(int i = 0 ; i < A.length - 1 ; i++){
+            int maxDistance = max(Math.abs(A[i] - A[i+1]), Math.abs(B[i] - B[i+1]));
+            distance += maxDistance;
+        }
+        return distance;
+    }
+
+    private int max(int a, int b){
+        if(a > b)
+            return a;
+        return b;
+
+    }
+}
