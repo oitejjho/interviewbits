d3c22a044560a72e18ba355cac0fbf32d074d397
Oitejjho Dutta <oitejjhodutta@AGC-FVFD703AP3YV.local>
2021-02-01 15:39:08 +0700

    FirstMissingInteger problem.

---
+++ src/com/company/FirstMissingInteger.java
@@ -0,0 +1,25 @@
+package company;
+
+public class FirstMissingInteger {
+    public int firstMissingPositive(ArrayList<Integer> A) {
+        if(A == null)
+            return 1;
+        int size = A.size();
+        for (int i = 0; i < size; i++) {
+            if (A.get(i) <= 0)
+                A.set(i, size+2);
+        }
+        for (int i = 0; i < size; i++) {
+            if(Math.abs(A.get(i)) <= size) {
+                int val = Math.abs(A.get(i));
+                val = Math.abs(A.get(val-1));
+                A.set(Math.abs(A.get(i)) - 1, -val);
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            if (A.get(i) > 0)
+                return i + 1;
+        }
+        return size + 1;
+    }
+}
